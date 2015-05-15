3ffa4b2e3aabe7c7c48ea06254e77e74d0f224b8
Claudia Tejos <claudia_tejos@hotmail.com>
2015-05-14 21:13:07 -0300

    listar unidade 2

---
+++ Uyzardi/src/java/br/senac/tads/pi3/uyzardi/ListarUnidadeServlet.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.uyzardi;
+
+import br.senac.tads.pi3.comum.ConnMysql;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author claudia.rgtejos
+ */
+@WebServlet(name = "ListarUnidadeServlet", urlPatterns = {"/ListarUnidadeServlet"})
+public class ListarUnidadeServlet extends HttpServlet {
+    
+      public ArrayList<Unidade> pesquisarUnidade (String pesquisa){
+        ArrayList<Unidade> listaUnidade = new ArrayList<>();
+        ResultSet resultados = null;
+        Statement stmt = null;
+        Connection conn = null;
+        
+        String sql;
+        if (pesquisa.equals("")) {
+             sql = "SELECT * FROM Unidade";
+        }
+        else{
+            sql = "SELECT * FROM `Unidade` WHERE `cidade` LIKE '%"+pesquisa+"%'";
+        }
+        try {
+            conn = ConnMysql.getConnection();
+            stmt = conn.prepareStatement(sql);
+            resultados = stmt.executeQuery(sql);
+            
+            if (resultados != null) {
+                while (resultados.next()){
+                    Unidade unidade = new Unidade(resultados.getInt("idUnidade"),
+                            resultados.getString("nomeUnidade"),
+                            resultados.getString("enderecoUnidade"),
+                            resultados.getString("cidade")
+                    );
+                    listaUnidade.add(unidade);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PesquisarClienteServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            if(stmt != null){
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ConnMysql.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ConnMysql.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+        return listaUnidade;
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ListarUnidadeServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            ArrayList<Unidade> lista = pesquisarUnidade("");
+            for (int i = 0; i < lista.size(); i++) {
+                //out.print(lista.get(i).getIdUnidade());
+                out.print(lista.get(i).getNome());
+                out.print(lista.get(i).getEndereco());
+            out.println("</body>");
+            out.println("</html>");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setAttribute("listaUnidade", pesquisarUnidade((String) request.getParameter("cidadeUnidade")));
+        //request.setAttribute("clickBtnPesquisaUnidade","true");
+        RequestDispatcher rd = request.getRequestDispatcher("telaPrincipal.jsp");
+        rd.forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
